9bc00d88b2efa1d7041e0e6764d050833aa430ea
jiangxd <806089486@qq.com>
2023-02-07 18:49:05 +0800

    log

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import requests
 import time
 import wechat
+from datetime import datetime
 
 logging.basicConfig(filename="monitor", format='%(asctime)s - %(name)s - %(levelname)s -%(module)s:  %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S ',
@@ -20,19 +21,26 @@ if __name__ == '__main__':
         configs = yaml.load(f, Loader=yaml.FullLoader)  # 按字典格式读取并返回
 
     if bool(configs["monitor"]["status"]) == True:
+        cnt = 1
         while True:
+            try:
+                for url in configs["monitor"]["urls"]:
+                    req = requests.get(url=url['url'])
 
-            for url in configs["monitor"]["urls"]:
-                req = requests.get(url=url['url'])
+                    if int(req.status_code) == 200:
+                        res = req.content
+                        if str(url['keyword']) not in str(res):
+                            senWx = wechat.SendWeiXinWork()
+                            senWx.send_message(url['name'] + "到货了\n" + url['url'])
+                            logger.info(
+                                str(datetime.now()) + " 次数:" + str(cnt) + "," + url['name'] + "到货了\n" + url['url'])
+                        else:
+                            if bool(configs["monitor"]["log"]) == True:
+                                logger.warning(
+                                    str(datetime.now()) + " 次数:" + str(cnt) + "," + url['name'] + " 缺货，持续刷新中（" +
+                                    url['keyword'] + "）")
 
-                if int(req.status_code) == 200:
-                    res = req.content
-                    if str(url['keyword']) not in str(res):
-                        senWx = wechat.SendWeiXinWork()
-                        senWx.send_message(url['name'] + "到货了\n" + url['url'])
-                        logger.info(url['name'] + "到货了\n" + url['url'])
-                    else:
-                        if bool(configs["monitor"]["log"]) == True:
-                            logger.warning(url['name'] + " 缺货，持续刷新中（" + url['keyword'] + "）")
-
-                time.sleep(int(configs["monitor"]["time"]))
+                    cnt = cnt + 1
+                    time.sleep(int(configs["monitor"]["time"]))
+            except Exception as err:
+                print(str(err))
